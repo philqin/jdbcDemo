7df7c38a1203aba3f6c9ef3173486972407e93f9
evol <1208369515@qq.com>
2022-10-12 16:44:10 +0800

    jdbc使用连接池方式web项目初始化

--- jdbcBase/src/main/java/com/evol/jdbc/JdbcBase.java
+++ jdbcBase/src/main/java/com/evol/jdbc/JdbcBase.java
@@ -44,7 +44,7 @@ public class JdbcBase {
         String sql = "select * from user";
 
         //TODO 5执行sql.并且返回结果集
-        ResultSet resultSet = statement.executeQuery(sql);
+        ResultSet resultSet = statement.executeQuery(sql);//可能出现sql注入的攻击
         //TODO 6对结果处理
         while (resultSet.next()){
             System.out.println(resultSet.getString("name"));
@@ -53,7 +53,37 @@ public class JdbcBase {
         //TODO 7关闭相关资源。先开后关
         resultSet.close();
         statement.close();
+
+       /* //TODO update 操作
+        connection.setAutoCommit(false);//开启事务
+        PreparedStatement up = connection.prepareStatement("update user set name=? where id =?");
+        up.setString(1,"覃玉炫");
+        up.setInt(2,1);
+        Boolean exe = up.execute();
+        //TODO 提交事务
+        connection.commit();
+        //TODO 回滚事务
+        //connection.rollback();
+        System.out.println("修改状态"+exe);
+        up.close();*/
+
+        //TODO 重复5、6的操作
+        PreparedStatement per = connection.prepareStatement("select * from user where id=?");
+        per.setInt(1,1);
+        ResultSet perS = per.executeQuery();
+        while (perS.next()){
+            System.out.println(perS.getString("name"));
+        }
+
+
+        //TODO 7关闭相关资源。先开后关
+        perS.close();
+        per.close();
+
         connection.close();
+
+        //TODO 都在使用通过个connection、创建了多个查询和执行对象
+
         return connection;
     }
 }
--- jdbcBase/src/main/java/com/evol/jdbc/JdbcOp.java
+++
@@ -1,5 +0,0 @@
-package com.evol.jdbc;
-
-public class JdbcOp {
-
-}
